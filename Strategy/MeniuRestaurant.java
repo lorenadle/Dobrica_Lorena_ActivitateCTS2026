89a8ea59906646e90e1fa5583683be99278cc404
lorenadle <DOBRICALORENA12@stud.ase.ro>
2026-05-14 18:05:53 +0300

    modif strategy

--- Strategy/MeniuRestaurant.java
+++ Strategy/MeniuRestaurant.java
@@ -2,21 +2,18 @@ package DP.Comportamentale.Strategy;
 
 import java.util.ArrayList;
 
-//clasa Context/Obiect din diagramele GoF
 public class MeniuRestaurant {
 	private ArrayList<OfertaMeniu> listaMeniuri = new ArrayList();
-	private IProcesabil strategieAlegere; //referinta strategiei curente
+	private IProcesabil strategieAlegere;
 	
 	public void addOfertaMeniu(OfertaMeniu ofertaMeniu){
 		this.listaMeniuri.add(ofertaMeniu);
 	}
 
-	//meth care permite schimbarea strategiei curente
-	public void setStrategieAlegere(IProcesabil strategieAlegere){
+public void setStrategieAlegere(IProcesabil strategieAlegere){
 		this.strategieAlegere = strategieAlegere;
 	}
 
-	//meth care, pe baza strategiei curente, "rezolva cerinte principala, adica alegerea unui meniu"
 	public OfertaMeniu alegereOferta(){
 		if(strategieAlegere != null){
 			return strategieAlegere.alegereMeniu(listaMeniuri);
--- Strategy/Program.java
+++ Strategy/Program.java
@@ -7,7 +7,7 @@ public class Program {
 	public static void main(String[] args) {
 		MeniuRestaurant meniuRestaurant = new MeniuRestaurant();
 		
-		//adaugare meniuri in meniu restaurant
+
 		OfertaMeniu meniu1 = new OfertaMeniu("Meniu1", 750, 70);
 		OfertaMeniu meniu2 = new OfertaMeniu("Meniu2", 500, 80);
 		OfertaMeniu meniu3 = new OfertaMeniu("Meniu3", 650, 20);
@@ -18,11 +18,11 @@ public class Program {
 		meniuRestaurant.addOfertaMeniu(meniu3);
 		meniuRestaurant.addOfertaMeniu(meniu4);
 		
-		//alegere meniu nr calorii minim
+
 		meniuRestaurant.setStrategieAlegere(new StrategieCaloriiMinim());
 		System.out.println("Meniu cu minim calorii " + meniuRestaurant.alegereOferta());
 	
-		//alegere meniu nr carbohidrati minim
+
 		meniuRestaurant.setStrategieAlegere(new StrategieCarbohidratiMinim());
 		System.out.println("Meniu cu minim carbohidrati " + meniuRestaurant.alegereOferta());
 
