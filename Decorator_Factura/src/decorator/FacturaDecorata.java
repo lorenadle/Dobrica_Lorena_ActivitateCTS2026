1eeeb1c19ef05b5f564d61808455f4bdbf76e608
lorenadle <DOBRICALORENA12@stud.ase.ro>
2026-05-14 18:15:50 +0300

    add modif decorator

--- Decorator_Factura/src/decorator/FacturaDecorata.java
+++ Decorator_Factura/src/decorator/FacturaDecorata.java
@@ -1,4 +1,16 @@
 package decorator;
 
-public class FacturaDecorata {
+import clase.Factura;
+
+public class FacturaDecorata extends Decorator{
+    public FacturaDecorata(Factura factura) {
+        super(factura);
+    }
+
+    @Override
+    public void aplicaDiscount(int procent) {
+        super.factura.setSumaFactura(super.factura.getSumaFactura()*(100-procent)/100);
+    }
+
+
 }
